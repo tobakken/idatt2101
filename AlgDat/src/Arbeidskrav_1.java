eea98015fd078cbe9914fc9b3c28a5cb2b5fa7ff
tobakken <tobakken@stud.ntnu.no>
2021-08-30 17:57:30 +0200

    Removed stonkCheck; added comments to the methods

--- AlgDat/src/Arbeidskrav_1.java
+++ AlgDat/src/Arbeidskrav_1.java
@@ -3,20 +3,31 @@ import java.util.Random;
 
 public class Arbeidskrav_1 {
     public static void main(String[] args) {
+        //int[] change = {-1, 3, -9, 2, 2, -1, -2, -1, 5};
+
+        //Lager en array med tilfeldige tall mellom -10 og 10 som representerer endringen i aksjekurs fra dag til dag.
+        //nSize bestemmer størrelsen på arrayen.
+        //Bruker den innebygde Random-funksjonen til Java, men denne gir problemer med større nSize. Algoritmen jeg
+        // bruker er likevel såpass treig at jeg ikke kommer opp i store nok n-verdier til at det gir for ulogiske svar.
         Random rand = new Random();
-        int nSize = 100000;
+        int nSize = 1000000;
+        System.out.println("N-size: " + nSize + "\n");
         int [] change = new int[nSize];
         for (int i = 0; i < nSize; i++) {
-            change[i] = (rand.nextInt(20) - 10);
+            change[i] = (rand.nextInt(21) - 10);
         }
 
         Date start = new Date();
-        System.out.println(stonkCheck(change));
+        System.out.println(stockCheck(change));
         Date slutt = new Date();
         System.out.println(slutt.getTime() - start.getTime());
     }
 
-    public static String stonkCheck(int[] priceChange){
+    /*
+    Totalt for metoden blir det O(n^2) siden det er for-loop i en for-loop.
+    Har 10 deklarasjoner og én sjekk O(10n), men disse har forsvinnende liten påvirkning om n-> uendelig.
+    */
+    public static String stockCheck(int[] priceChange) {
         int bestBuyDay = 0;
         int bestSellDay = 0;
         int price = 0;
@@ -24,9 +35,10 @@ public class Arbeidskrav_1 {
         int bestSum = 0;
         int sum;
 
-        for (int i = 0; i < priceChange.length; i++) {
+
+        for (int i = 0; i < priceChange.length; i++) { //O(n)
             price += priceChange[i];
-            for (int j = i; j < (priceChange.length - i); j++) {
+            for (int j = i; j < (priceChange.length - i); j++) { //O(n)
                 priceCheck += priceChange[j];
                 sum = priceCheck - price;
                 if (sum > bestSum) {
