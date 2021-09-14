45dd8da3f69ac20a177542ec0e8c2563929c890d
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2021-09-14 10:58:11 +0200

    Complete Arbeidskrav_3

--- AlgDat/src/Arbeidskrav_3.java
+++ AlgDat/src/Arbeidskrav_3.java
@@ -5,63 +5,61 @@ import java.util.Random;
 public class Arbeidskrav_3 {
     public static void main(String[] args) {
 
-/*        int [] testTabell = {5,2,7,6,8,9};
-        boblesort(testTabell);
-        System.out.println(Arrays.toString(testTabell));*/
-
-        int tabellsize = 20;
+        int tabellsize = 1000000;
+        int randomRange = 500000;
 
         Random rand = new Random();
-        int [] tabell = new int[tabellsize];
+        int[] tabell = new int[tabellsize];
         for (int i = 0; i < tabell.length; i++) {
-            tabell[i] = rand.nextInt(tabellsize);
+            tabell[i] = rand.nextInt(randomRange);
         }
 
-        System.out.println(sum(tabell));
+        System.out.println("Range of random numbers: " + randomRange);
+        System.out.println("Sum: " + sum(tabell) + "\n");
 
         //Lager en kopi for sammenligning
-        int [] tabellKopi1 = Arrays.copyOf(tabell, tabellsize);
+        int[] tabellKopi1 = Arrays.copyOf(tabell, tabellsize);
 
         System.out.println(tabellTestRiktigSortert(tabell));
         System.out.println(tidtaking(tabell, tabellsize));
         System.out.println(sum(tabell));
 
         System.out.println("\n" + tabellTestRiktigSortert(tabellKopi1));
-        System.out.println(tidtakingImprovedQS(tabellKopi1, tabellsize, 10));
+        System.out.println(tidtakingImprovedQS(tabellKopi1, tabellsize));
         System.out.println(sum(tabellKopi1));
     }
 
     //Tidtaking av vanlig quicksort
-    private static String tidtaking(int [] tabell, int size){
+    private static String tidtaking(int[] tabell, int size) {
         Date start = new Date();
-        quicksort(tabell, 0, size-1);
+        quicksort(tabell, 0, size - 1);
         Date slutt = new Date();
         Double tid = (double)
-                (slutt.getTime()-start.getTime());
+                (slutt.getTime() - start.getTime());
         return String.format("Vanlig quicksort: \nMillisekund brukt: %.7f%n", tid) + ("Tabellen er sortert rett: " + tabellTestRiktigSortert(tabell));
     }
 
-    //Tidtaking av quicksort med hjelpemetode
-    private static String tidtakingImprovedQS(int [] tabell, int size, int brytRekSize){
+    //Tidtaking av forbedret quicksort
+    private static String tidtakingImprovedQS(int[] tabell, int size) {
         Date start = new Date();
-        quicksortImproved(tabell, 0, size-1);
+        quicksortImproved(tabell, 0, size - 1);
         Date slutt = new Date();
         Double tid = (double)
-                (slutt.getTime()-start.getTime());
-        return String.format("Bryter rekursjon på " + brytRekSize + "\nQuicksort med hjelpealgoritme: \nMillisekund brukt: %.7f%n", tid) +
+                (slutt.getTime() - start.getTime());
+        return String.format("Quicksort med hjelpealgoritme: \nMillisekund brukt: %.7f%n", tid) +
                 ("Tabellen er sortert rett: " + tabellTestRiktigSortert(tabell));
     }
 
-    public static void bytt(int []t, int v, int m){
+    public static void bytt(int[] t, int v, int m) {
         int hjelp = t[v];
         t[v] = t[m];
         t[m] = hjelp;
     }
 
-    public static int median3sort(int []t, int v, int h){
-        int m = (v+h)/2;
-        if(t[v] > t[m]) bytt(t, v, m);
-        if(t[m] > t[h]) {
+    public static int median3sort(int[] t, int v, int h) {
+        int m = (v + h) / 2;
+        if (t[v] > t[m]) bytt(t, v, m);
+        if (t[m] > t[h]) {
             bytt(t, m, h);
             if (t[v] > t[m]) bytt(t, v, m);
         }
@@ -69,31 +67,35 @@ public class Arbeidskrav_3 {
     }
 
     //100% Quicksort slik den står i lærebok
-    public static void quicksort(int []t, int v, int h){
-        if (h - v > 2){
+    public static void quicksort(int[] t, int v, int h) {
+        if (h - v > 2) {
             int delepos = splitt(t, v, h);
             quicksort(t, v, delepos - 1);
             quicksort(t, delepos + 1, h);
         } else median3sort(t, v, h);
     }
 
-    //Quicksort med annen algoritme for små deltabeller
-    public static void quicksortImproved(int []t, int v, int h){
-        if (h - v > 10){
-            int delepos = splitt(t, v, h);
-            quicksortImproved(t, v, delepos - 1);
-            quicksortImproved(t, delepos + 1, h);
-        } else boblesort(t, h, v);
+    //Quicksort med sjekk for like elementer i tabell
+    public static void quicksortImproved(int[] t, int v, int h) {
+        if (v > 0 && h < t.length - 1 && t[v - 1] == t[h + 1]) {}
+        else {
+            if (h - v > 2) {
+                int delepos = splitt(t, v, h);
+                quicksortImproved(t, v, delepos - 1);
+                quicksortImproved(t, delepos + 1, h);
+            } else median3sort(t, v, h);
+        }
+
     }
 
-    private static int splitt(int []t, int v, int h){
+    private static int splitt(int[] t, int v, int h) {
         int iv, ih;
         int m = median3sort(t, v, h);
         int dv = t[m];
-        bytt(t, m, h -1);
-        for (iv = v, ih = h - 1;;){
-            while (t[++iv] < dv);
-            while (t[--ih] > dv);
+        bytt(t, m, h - 1);
+        for (iv = v, ih = h - 1; ; ) {
+            while (t[++iv] < dv) ;
+            while (t[--ih] > dv) ;
             if (iv >= ih) break;
             bytt(t, iv, ih);
         }
@@ -101,26 +103,38 @@ public class Arbeidskrav_3 {
         return iv;
     }
 
-    public static void boblesort(int[] t, int start, int stop){
-        for (int i = start -1; i>stop; --i) {
+    public static void boblesort(int[] t, int start, int stop) {
+        for (int i = start - 1; i > stop; --i) {
             for (int j = 0; j < i; j++) {
-                if (t[j] > t[j+1]){
-                    bytt(t, j, j+1);
+                if (t[j] > t[j + 1]) {
+                    bytt(t, j, j + 1);
                 }
             }
         }
     }
 
-    public static boolean tabellTestRiktigSortert(int []t){
+    public static void innsettingssort(int[] t, int start, int length) {
+        for (int i = start; i < length; i++) {
+            int bytt = t[i];
+            int j = i - 1;
+            while (j >= 0 && t[j] > bytt) {
+                t[j + 1] = t[j];
+                --j;
+            }
+            t[j + 1] = bytt;
+        }
+    }
+
+    public static boolean tabellTestRiktigSortert(int[] t) {
         for (int i = 0; i < (t.length - 1); i++) {
-            if (t[i] > t[i+1]){
+            if (t[i] > t[i + 1]) {
                 return false;
             }
         }
         return true;
     }
 
-    private static int sum(int[] t){
+    private static int sum(int[] t) {
         int sum = 0;
         for (int j : t) {
             sum += j;
