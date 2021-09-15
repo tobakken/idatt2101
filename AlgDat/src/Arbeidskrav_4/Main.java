99babf6a49696455370bcdffa67eaea09423ef64
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2021-09-15 16:26:57 +0200

    Fixed arbeidskrav 4 oppgave 1

--- AlgDat/src/Arbeidskrav_4/Main.java
+++ AlgDat/src/Arbeidskrav_4/Main.java
@@ -1,62 +1,32 @@
-public class Arbeidskrav_4 {
+package Arbeidskrav_4;
+
+public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Den trygge plassen er: " + josephus(10, 4));
     }
 
-
-}
-
-class Node {
-    double element;
-    Node neste;
-    Node forrige;
-
-    public Node(double e, Node n, Node f){
-        this.element = e;
-        this.neste = n;
-        this.forrige = f;
-    }
-
-    public double getElement() {
-        return element;
-    }
-
-    public Node getNeste() {
-        return neste;
-    }
-
-    public Node getForrige() {
-        return forrige;
-    }
-}
-
-class SirkulaerListe {
-    private Node hode = null;
-    private Node hale = null;
-    private int antElementer = 0;
-    public int getAntElementer() {
-        return antElementer;
-    }
-
-    public void leggTilNode(double verdi){
-
-    }
-
-    public void settInnFremst (double verdi){
-        hode = new Node(verdi, hode, null);
-        if (hode == null) hale = hode;
-        else hode.neste.forrige = hode;
-        ++antElementer;
-    }
-
-    public void settInnBakerst(double verdi){
-        Node ny = new Node(verdi, null, hale);
-        if (hale != null) hale.neste = ny;
-        else {
-            hode = ny;
-            hale = ny;
+    public static double josephus(int n, int m){
+        Node first = new Node(1);
+        Node prev = first;
+        for (int i = 2; i < n+1; i++) {
+            Node ny = new Node(i);
+            ny.forrige = prev;
+            prev.neste = ny;
+            prev = ny;
+            first.forrige = ny;
         }
+        prev.neste = first;
 
-        ++antElementer;
+        Node current = first.forrige;
+
+        //Hvis den nåverende noden linker til seg selv må det bety at det kun er igjen én person.
+        while (!current.equals(current.neste)){
+            //steger seg gjennom personer som er trygge før den hopper over den som blir drept.
+            for (int i = 0; i < m-1; i++) {
+                current = current.neste;
+            }
+            current.neste = current.neste.neste;
+        }
+        return current.getElement();
     }
 }
--- AlgDat/src/Arbeidskrav_4/Node.java
+++ AlgDat/src/Arbeidskrav_4/Node.java
@@ -1,2 +1,31 @@
-package Arbeidskrav_4;public class Node {
+package Arbeidskrav_4;
+
+public class Node {
+    double element;
+    Node neste;
+    Node forrige;
+
+    public Node(double e){
+        this.element = e;
+    }
+
+    public double getElement() {
+        return element;
+    }
+
+    public Node getNeste() {
+        return neste;
+    }
+
+    public Node getForrige() {
+        return forrige;
+    }
+
+    public void setNeste(Node neste) {
+        this.neste = neste;
+    }
+
+    public void setForrige(Node forrige) {
+        this.forrige = forrige;
+    }
 }
