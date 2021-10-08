9ddb9d4eb2d0f8a3b0d47ec30a16ede24babf24c
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2021-10-08 11:50:44 +0200

    Ak6oppg2 andre forsøk

--- AlgDat/src/Arbeidskrav_6/AK6OPPG2.java
+++ AlgDat/src/Arbeidskrav_6/AK6OPPG2.java
@@ -1,17 +1,36 @@
 package Arbeidskrav_6;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class AK6OPPG2 {
     public static void main(String[] args) {
-        Graf graf = new Graf(3);
-        graf.leggTilKanter(0, 2);
-        graf.leggTilKanter(2, 1);
+        Graf graf = new Graf(0);
+        String line;
+        ArrayList<String[]> splitt = new ArrayList<>();
+        File file = new File("./l7g5.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while ((line = br.readLine()) != null) {
+                String trimmet = line.trim();
+                splitt.add(trimmet.split(" "));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        graf = new Graf(Integer.parseInt(splitt.get(0)[0]));
+        for (int i = 1; i < splitt.size(); i++) {
+            graf.leggTilKanter(Integer.parseInt(splitt.get(i)[0]), Integer.parseInt(splitt.get(i)[1]));
+        }
 
-        ArrayList<Node> topores = graf.topologisort();
+        Node topores = graf.toposortFu();
 
-        System.out.println(topores.toString());
+        for (int i = 0; i < graf.node.length; i++) {
+            System.out.print(" " + topores);
+            topores = topores.neste;
+        }
     }
 }
 
@@ -19,6 +38,7 @@ class Node {
     int element;
     ArrayList<Node> kanter;
     boolean funnet;
+    Node neste;
 
     public Node(int n) {
         element = n;
@@ -27,7 +47,7 @@ class Node {
     }
 
     public void setKanter(Node k) {
-        if (!kanter.contains(k)){
+        if (!kanter.contains(k)) {
             kanter.add(k);
         }
     }
@@ -49,26 +69,25 @@ class Graf {
         }
     }
 
-    private void toposort(Node n, ArrayList<Node> list, int j){
-        n.funnet = true;
+    private Node toposortFungerer(Node n, Node l) {
+        if (n.funnet) return l;
         for (int i = 0; i < n.kanter.size(); i++) {
             Node sjekk = n.kanter.get(i);
-            if (!sjekk.funnet){
-                toposort(sjekk, list, j+1);
-                sjekk.funnet = true;
-            }
+            l = toposortFungerer(sjekk, l);
         }
-        if (!list.contains(n)) list.add(n);
+        n.funnet = true;
+        n.neste = l;
+        return n;
     }
 
-    public ArrayList<Node> topologisort() {
-        ArrayList<Node> sortert = new ArrayList<>();
-        for (int i = node.length; i-- >0; ){
-            toposort(node[i], sortert, 0);
+    public Node toposortFu() {
+        Node l = null;
+        for (int i = node.length; i-- > 0; ) {
+            l = toposortFungerer(node[i], l);
         }
-        Collections.reverse(sortert);
-        return sortert;
+        return l;
     }
+
 
     public void leggTilKanter(int h, int n) {
         Node hode = node[h];
