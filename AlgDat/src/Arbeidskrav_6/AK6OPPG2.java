6843b120e894094a674e2a6e399b4a5f6c39f8e2
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2021-10-08 14:45:28 +0200

    AK6 Oppgave 2 fullført

--- AlgDat/src/Arbeidskrav_6/AK6OPPG2.java
+++ AlgDat/src/Arbeidskrav_6/AK6OPPG2.java
@@ -4,33 +4,38 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Collections;
+
 
 public class AK6OPPG2 {
     public static void main(String[] args) {
-        Graf graf = new Graf(0);
-        String line;
-        ArrayList<String[]> splitt = new ArrayList<>();
-        File file = new File("./l7g5.txt");
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            while ((line = br.readLine()) != null) {
-                String trimmet = line.trim();
-                splitt.add(trimmet.split(" "));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Graf graf;
+        ArrayList<String []> splitt = lesFil("L7g5");
         graf = new Graf(Integer.parseInt(splitt.get(0)[0]));
         for (int i = 1; i < splitt.size(); i++) {
             graf.leggTilKanter(Integer.parseInt(splitt.get(i)[0]), Integer.parseInt(splitt.get(i)[1]));
         }
 
-        Node topores = graf.toposortFu();
+        Node topores = graf.toposort();
 
         for (int i = 0; i < graf.node.length; i++) {
             System.out.print(" " + topores);
             topores = topores.neste;
         }
+    }
+
+    public static ArrayList<String[]> lesFil(String filnavn){
+        String line;
+        ArrayList<String[]> splitt = new ArrayList<>();
+        File file = new File("./" + filnavn + ".txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while ((line = br.readLine()) != null) {
+                String trimmet = line.trim();
+                splitt.add(trimmet.split("\\s+"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return splitt;
     }
 }
 
@@ -69,21 +74,21 @@ class Graf {
         }
     }
 
-    private Node toposortFungerer(Node n, Node l) {
+    private Node toposortHjelp(Node n, Node l) {
         if (n.funnet) return l;
         for (int i = 0; i < n.kanter.size(); i++) {
             Node sjekk = n.kanter.get(i);
-            l = toposortFungerer(sjekk, l);
+            l = toposortHjelp(sjekk, l);
         }
         n.funnet = true;
         n.neste = l;
         return n;
     }
 
-    public Node toposortFu() {
+    public Node toposort() {
         Node l = null;
         for (int i = node.length; i-- > 0; ) {
-            l = toposortFungerer(node[i], l);
+            l = toposortHjelp(node[i], l);
         }
         return l;
     }
