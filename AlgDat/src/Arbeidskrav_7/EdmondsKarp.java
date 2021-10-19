c7fd2ceda3b431b480a004c34936de1536381b4f
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2021-10-19 12:07:59 +0200

    Update gitignore; Development of edmKarp-method

--- AlgDat/src/Arbeidskrav_7/EdmondsKarp.java
+++ AlgDat/src/Arbeidskrav_7/EdmondsKarp.java
@@ -9,7 +9,8 @@ public class EdmondsKarp {
     public static void main(String[] args) {
 
     }
-    public static void readFile(String filnavn){
+
+    public static void readFile(String filnavn) {
         String line;
         String[] readNum;
         File file = new File("./" + filnavn + ".txt");
@@ -19,7 +20,7 @@ public class EdmondsKarp {
 
             //Read rest of file and add nodes/edges
             while ((line = br.readLine()) != null) {
-            readNum = br.readLine().trim().split("\\s+");
+                readNum = br.readLine().trim().split("\\s+");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,13 +32,20 @@ public class EdmondsKarp {
 class Node {
     int element;
     ArrayList<Edge> edges;
+    boolean visited;
+    int distance;
 
 
     public Node(int n) {
         element = n;
         edges = new ArrayList<>();
+        visited = false;
     }
 
+    public void addEdge(Node neighbour, int flow) {
+        edges.add(new Edge(neighbour, flow));
+        neighbour.edges.add(new Edge(this, 0));
+    }
 
     @Override
     public String toString() {
@@ -56,22 +64,72 @@ class Graph {
         }
     }
 
-    public void addEdge(int h, int n, int flow) {
+    public void addEdge(int h, int n, int cap) {
         Node head = node[h];
         Node next = node[n];
+        head.addEdge(next, cap);
+    }
+
+    public void edmKarp(int s, int drain) {
+        Stack queue = new Stack();
+        int increase = 1000000;
+        int dist = 0;
+        Node check = node[s];
+        queue.push(s);
+        check.visited = true;
+        while (!queue.empty() && !check.equals(node[drain])) {
+            dist++;
+            for (int i = check.edges.size(); --i > 0; ) {
+                check = check.edges.get(i).end;
+                check.distance = dist;
+                if (!check.visited) {
+                    queue.push(check.element);
+                    check.visited = true;
+                }
+            }
+            check = node[queue.next()];
+        }
     }
 }
 
 class Edge {
-    Node start;
     Node end;
     int flow;
+    int cap;
 
-    public Edge(Node s, Node e, int flow){
-        this.start = s;
+    public Edge(Node e, int cap) {
         this.end = e;
-        this.flow = flow;
+        this.cap = cap;
     }
 
+    public int restCap() {
+        return cap - flow;
+    }
+}
 
+class Stack {
+
+    ArrayList<Integer> stack;
+
+    public Stack() {
+        stack = new ArrayList<>();
+    }
+
+    public void push(int n) {
+        stack.add(n);
+    }
+
+    public boolean empty() {
+        return stack.size() == 0;
+    }
+
+    public int next() {
+        int x = stack.get(0);
+        stack.remove(0);
+        return x;
+    }
+
+    public String toString() {
+        return this.stack + " ";
+    }
 }
